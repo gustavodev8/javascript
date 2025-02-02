62ab3f6e9ee2d371b92e442c4af56e5c20459065
gustavodev8 <gustavu.dev@gmail.com>
2025-02-02 01:24:22 -0300

    updates do estudo de java

---
+++ ex 13/desafio.js
@@ -0,0 +1,13 @@
+let desafio = prompt ('fale algum numero')
+document.body.innerHTML += '<h1> seu numero é '  + desafio +  '</h1>'
+desafio = parseFloat(desafio.toUpperCase(2))
+let resultado = (Number.isInteger(desafio)) 
+document.body.innerHTML += 'o seu numero é <strong>inteiro</strong>?  ' + resultado + '<br>'
+let raiz = (Math.sqrt(desafio)) 
+document.body.innerHTML += 'a raiz quadrada de seu numero é: ' + raiz + '<br>'
+let arredondado = Math.round(desafio)
+document.body.innerHTML += 'numero arredondado para cima: ' +arredondado+ ' <br>'
+let parabaixo = Math.floor (desafio)
+document.body.innerHTML += 'seu numero arredondado para baixo: ' +parabaixo+ '<br>'
+let metade = (desafio/2)
+document.body.innerHTML += desafio + ' divido pela metade é ' + metade
