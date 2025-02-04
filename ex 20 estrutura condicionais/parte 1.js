c047daaf211829e834e68bff70abe8a33c0268dd
gustavodev8 <gustavu.dev@gmail.com>
2025-02-04 13:00:36 -0300

    update

---
+++ ex 20 estrutura condicionais/parte 1.js
@@ -0,0 +1,23 @@
+const hora = 20;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('bom dia')
+} else if (hora >= 12 && hora <= 17) {
+    console.log('boa tarde')
+} else if (hora >= 18 && hora <= 24) {
+    console.log('boa noite')
+} else if (hora > 24) {
+    console.log('este horario nao existe')
+}
+
+const dinheiro = 32;
+
+if (dinheiro > 50) {
+    console.log ('eu vou sair de casa')
+} else {
+    console.log ('eu vou ficar em casa')
+}
+
+// pode usar o else sem o if, ou o contrario
+
+const numero = 0 
---
+++ ex 20 estrutura condicionais/parte 2.js
@@ -0,0 +1,15 @@
+const numero = 21;
+
+if (numero >= 10 && numero <= 20) {                     // SE
+    console.log ('o numero esta entre 0 e 20!')         
+} else {                                                // SE NÃO
+console.log ('o numero não esta entre 0 e 20!')
+}
+
+console.log ('')
+
+if (numero >= 10 && numero <= 20) {                     // SE
+    console.log ('o numero esta entre 0 e 20!')         
+} else if (numero > 20){                                // SE NÃO SE
+console.log ('logico que é ne seu burro')
+}
