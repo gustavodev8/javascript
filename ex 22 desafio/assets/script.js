2c14bc25b9c4dab9453fe2e9b556981496174b62
gustavodev8 <gustavu.dev@gmail.com>
2025-02-08 00:20:16 -0300

    update

--- ex 22 desafio/assets/script.js
+++ ex 22 desafio/assets/script.js
@@ -1,13 +1,50 @@
-const section = document.querySelector('.container')
-section.addEventListener('submit', function (evento)) {
-evento.preventDefault()
-imcdado('ola mundo')
+const form = document.querySelector('.form')
+
+
+
+
+
+
+form.addEventListener('submit', function (e) { // assistir evento
+    e.preventDefault() // previnir evento
+
+    const inputPESO = e.target.querySelector('.peso')
+    const inputALT = e.target.querySelector('.altura')
+
+    const peso = Number(inputPESO.value)
+    const altura = Number(inputALT.value)
+
+    if (!peso) { // se o peso for valido isso n executa por causa do !
+        resultado('peso invalido', false)
+        return;
+    }
+    if (!altura) { //se o altura for valido isso n executa por causa do !
+        resultado('altura invalido', false)
+        return;
+    }
+
+    const imc = getimc(peso, altura)
+    console.log (imc)
+})
+
+function getimc (peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2) // retornar com 2 casas decimais
+
 }
 
+function criaP () {
+    const p = document.createElement('p'); // isso cria um novo elemento e adiciona nele um css mesmo
+    return p;
+}
+function resultado (msg, isvalid) {
+    const resultado = document.querySelector ('.resultadoP')
+    resultado.innerHTML = '';
 
-    function imcdado1 (msg) {
-        const completo = document.querySelector('#imcdado')
-        completo.innerHTML = msg
-    }
+    const p = criaP()
+    p.innerHTML = (msg)
+    resultado.appendChild(p)
+}
+
 
 
